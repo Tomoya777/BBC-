125ac513f487517a70bcec05d4dd20a90819c1c3
Tomoya777 <77311003+Tomoya777@users.noreply.github.com>
2021-08-07 18:04:07 +0900

    blog1

--- summer/polls/views.py
+++ summer/polls/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
-def index_templates(request):
-    return render(request, 'index.html')
+
+def blog(request):
+    blogs = Blog.objects.order_by('-created_datetime')
+    return render(request, 'blog.html', {'blogs': blogs})
