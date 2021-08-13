2ed130cc0123f24e52db8bfe488c10dd1e772fab
Tomoya777 <77311003+Tomoya777@users.noreply.github.com>
2021-08-13 23:23:23 +0900

    連携

--- summer/polls/views.py
+++ summer/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Blog
 
+def toppage(request):
+    return render(request, 'toppage.html')
+    
 def blog(request):
     blogs = Blog.objects.order_by('-created_datetime')
     return render(request, 'blog.html', {'blogs': blogs})
