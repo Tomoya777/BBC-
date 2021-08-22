117b481d680d79f20e13327e05e803954432302c
Tomoya777 <77311003+Tomoya777@users.noreply.github.com>
2021-08-22 21:56:31 +0900

    blog完成

--- summer/polls/urls.py
+++ summer/polls/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.toppage, name='toppage'),
     path('blog/', views.blog, name='blog'),#リンクを追加するごとに作成
+    path('blog/detail/<int:blog_id>/', views.detail, name='detail'),
 ]
--- summer/polls/views.py
+++ summer/polls/views.py
@@ -7,3 +7,7 @@ def toppage(request):
 def blog(request):
     blogs = Blog.objects.order_by('-created_datetime')
     return render(request, 'blog.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    return render(request, 'detail.html', {'blog': blog})
